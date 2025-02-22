5160c379f077176b1f63f6d19a63d4d5217f6fad
Ashan Ekanayake <ashanekanayakeat@gmail.com>
2025-02-22 23:50:23 +0530

    footer add

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,11 +1,71 @@
-import React from 'react'
+import React from "react";
+import { Container, Row, Col,ListGroup,ListGroupItem} from "reactstrap";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "remixicon/fonts/remixicon.css";
+import "../../Styles/footer.css";
+const quickLinks=[
+  {
+    path:'/about',
+    display:'About'
+  },
+  {
+    path:'/#',
+    display:'Privacy Policy'
+  },
+  {
+    path:'/cars',
+    display:'Car Listing'
+  },
+  {
+    path:'/about',
+    display:'About'
+  },
+]
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg="4" md="4" sm="12">
+            <div className="logo">
+              <h1>
+                <Link to="/home" className="d-flex align-items-center gap-2">
+                  <i class="ri-car-line"></i>
+                  <span>
+                    Car Rental
+                    <br />
+                    Service
+                  </span>
+                </Link>
+              </h1>
+            </div>
+            <p className='footer__logo-content'>
+              ppppppppppppppppppppppppppppppppppppppp
+              </p>
+          </Col>
+         
+          <Col lg='4' md='4' sm='6'>
+          <div className="mb-4">
+          <h5 className="footer__link-title">Quick Links</h5>
+          <ListGroup>
+            {
+              quickLinks.map((item,index)=>(
+                <ListGroupItem key={index} className="p-0 mt-3">
+                  <Link to ={item.path}>{item.display}</Link>
 
-export default Footer
+                </ListGroupItem>
+              ))
+            }
+          </ListGroup>
+          </div>
+          
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
