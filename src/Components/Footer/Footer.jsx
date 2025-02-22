e7821ed8a2217f4536b3db864eee50be3700ec0b
Ashan Ekanayake <ashanekanayakeat@gmail.com>
2025-02-23 00:48:35 +0530

    footer updated

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -18,8 +18,12 @@ const quickLinks=[
     display:'Car Listing'
   },
   {
-    path:'/about',
-    display:'About'
+    path:'/blogs',
+    display:'Blog'
+  },
+  {
+    path:'/contact',
+    display:'Contact'
   },
 ]
 
@@ -42,11 +46,11 @@ const Footer = () => {
               </h1>
             </div>
             <p className='footer__logo-content'>
-              ppppppppppppppppppppppppppppppppppppppp
+            Lorem ipsum, placeholder or dummy text used in typesetting and graphic design for previewing layouts. It features scrambled Latin text, which emphasizes the design over content of the layout. It is the standard placeholder text of the printing and publishing industries.
               </p>
           </Col>
          
-          <Col lg='4' md='4' sm='6'>
+          <Col lg='2' md='4' sm='6'>
           <div className="mb-4">
           <h5 className="footer__link-title">Quick Links</h5>
           <ListGroup>
@@ -60,7 +64,29 @@ const Footer = () => {
             }
           </ListGroup>
           </div>
-          
+          </Col>
+
+          <Col lg='3' md='4' sm='6'>
+          <div className="mb-4">
+            <h5 className="footer__link-title">Head Office</h5>
+            <p className="office__info">112/4, Nicholos Road, Colombo 10, Sri Lanka</p>
+            <p className="office__info">Phone: 011-6597418</p>
+            <p className="office__info">Email: biverse@gmail.com</p>
+            <p className="office__info">Office Time:8am-6am</p>
+          </div>
+          </Col>
+
+          <Col lg='3' md='4' sm='6'>
+          <div className="mb-4">
+            <h5 className="footer__link-title">Newsletter</h5>
+            <p className="section__description">Subscribe our newsletter</p>
+            <div className="newsletter">
+              <input type="email" placeholder="Email" />
+              <span>
+                <i class="ri-send-plane-line"></i>
+                </span>
+            </div>
+          </div>
           </Col>
         </Row>
       </Container>
