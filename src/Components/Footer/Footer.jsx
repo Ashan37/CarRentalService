5d5a09011d912b469dcb8c2ef99b9769dc50a19d
Ashan Ekanayake <ashanekanayakeat@gmail.com>
2025-02-23 16:23:05 +0530

    footer CSS Update

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,33 +1,36 @@
 import React from "react";
-import { Container, Row, Col,ListGroup,ListGroupItem} from "reactstrap";
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
 import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "remixicon/fonts/remixicon.css";
 import "../../Styles/footer.css";
-const quickLinks=[
+const quickLinks = [
   {
-    path:'/about',
-    display:'About'
+    path: "/about",
+    display: "About",
   },
   {
-    path:'/#',
-    display:'Privacy Policy'
+    path: "/#",
+    display: "Privacy Policy",
   },
   {
-    path:'/cars',
-    display:'Car Listing'
+    path: "/cars",
+    display: "Car Listing",
   },
   {
-    path:'/blogs',
-    display:'Blog'
+    path: "/blogs",
+    display: "Blog",
   },
   {
-    path:'/contact',
-    display:'Contact'
+    path: "/contact",
+    display: "Contact",
   },
-]
+];
 
 const Footer = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+
   return (
     <footer className="footer">
       <Container>
@@ -45,48 +48,60 @@ const Footer = () => {
                 </Link>
               </h1>
             </div>
-            <p className='footer__logo-content'>
-            Lorem ipsum, placeholder or dummy text used in typesetting and graphic design for previewing layouts. It features scrambled Latin text, which emphasizes the design over content of the layout. It is the standard placeholder text of the printing and publishing industries.
-              </p>
-          </Col>
-         
-          <Col lg='2' md='4' sm='6'>
-          <div className="mb-4">
-          <h5 className="footer__link-title">Quick Links</h5>
-          <ListGroup>
-            {
-              quickLinks.map((item,index)=>(
-                <ListGroupItem key={index} className="p-0 mt-3 quick__link">
-                  <Link to ={item.path}>{item.display}</Link>
-
-                </ListGroupItem>
-              ))
-            }
-          </ListGroup>
-          </div>
+            <p className="footer__logo-content">
+              Our car rental service offers a hassle-free experience with a
+              variety of well-maintained vehicles at competitive rates. Whether
+              for business, travel, or daily use, we provide flexible rentals,
+              easy booking. Enjoy comfort, reliability, and
+              affordability—book your car today!
+            </p>
           </Col>
 
-          <Col lg='3' md='4' sm='6'>
-          <div className="mb-4">
-            <h5 className="footer__link-title mb-4">Head Office</h5>
-            <p className="office__info">112/4, Nicholos Road, Colombo 10, Sri Lanka</p>
-            <p className="office__info">Phone: 011-6597418</p>
-            <p className="office__info">Email: biverse@gmail.com</p>
-            <p className="office__info">Office Time:8am-6am</p>
-          </div>
-          </Col>
-
-          <Col lg='3' md='4' sm='6'>
-          <div className="mb-4">
-            <h5 className="footer__link-title">Newsletter</h5>
-            <p className="section__description">Subscribe our newsletter</p>
-            <div className="newsletter">
-              <input type="email" placeholder="Email" />
-              <span>
-                <i class="ri-send-plane-line"></i>
-                </span>
+          <Col lg="2" md="4" sm="6">
+            <div className="mb-4">
+              <h5 className="footer__link-title">Quick Links</h5>
+              <ListGroup>
+                {quickLinks.map((item, index) => (
+                  <ListGroupItem key={index} className="p-0 mt-3 quick__link">
+                    <Link to={item.path}>{item.display}</Link>
+                  </ListGroupItem>
+                ))}
+              </ListGroup>
             </div>
-          </div>
+          </Col>
+
+          <Col lg="3" md="4" sm="6">
+            <div className="mb-4">
+              <h5 className="footer__link-title mb-4">Head Office</h5>
+              <p className="office__info">
+                112/4, Nicholos Road, Colombo 10, Sri Lanka
+              </p>
+              <p className="office__info">Phone: 011-6597418</p>
+              <p className="office__info">Email: biversecar@gmail.com</p>
+              <p className="office__info">Office Time: 8am-6pm</p>
+            </div>
+          </Col>
+
+          <Col lg="3" md="4" sm="6">
+            <div className="mb-4">
+              <h5 className="footer__link-title">Newsletter</h5>
+              <p className="section__description">Subscribe our newsletter</p>
+              <div className="newsletter">
+                <input type="email" placeholder="Email" />
+                <span>
+                  <i class="ri-send-plane-line"></i>
+                </span>
+              </div>
+            </div>
+          </Col>
+
+          <Col lg="12">
+            <div className="footer__bottom">
+              <p className="section__description d-flex align-items-center justify-content-center gap-1 pt-4">
+                <i class="ri-copyright-line"></i>CopyRight {year}, Developed by
+                Ashan Ekanayake. All rights reserved.
+              </p>
+            </div>
           </Col>
         </Row>
       </Container>
